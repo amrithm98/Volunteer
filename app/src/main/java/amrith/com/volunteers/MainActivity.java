a601b80068293f0f5043cc1dc46d72f7b455b85e
amrithm98 <amrithchallenger@gmail.com>
2017-06-14 15:41:34 +0530

    drawer simple

--- app/src/main/java/amrith/com/volunteers/MainActivity.java
+++ app/src/main/java/amrith/com/volunteers/MainActivity.java
@@ -80,18 +80,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_feed) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_myEvents) {
 
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_tools) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
