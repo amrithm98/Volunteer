60e2b987fa5b33fa9fbf12f5548cfca75bcd143a
amrithm98 <amrithchallenger@gmail.com>
2017-06-14 16:19:08 +0530

    Login layout

--- app/src/main/java/amrith/com/volunteers/LoginActivity.java
+++ app/src/main/java/amrith/com/volunteers/LoginActivity.java
@@ -1,7 +1,10 @@
 package amrith.com.volunteers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login(View v)
+    {
+        Toast.makeText(getApplicationContext(),"Login",Toast.LENGTH_SHORT).show();
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
     }
 }
