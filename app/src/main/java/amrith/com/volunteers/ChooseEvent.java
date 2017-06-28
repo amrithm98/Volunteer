3e3a59c3134d2dd522176ad94cc3c0a2e4773a03
amrithm98 <amrithchallenger@gmail.com>
2017-06-28 17:57:52 +0530

    Event CHooser activity

---
+++ app/src/main/java/amrith/com/volunteers/ChooseEvent.java
@@ -0,0 +1,13 @@
+package amrith.com.volunteers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_event);
+    }
+}
