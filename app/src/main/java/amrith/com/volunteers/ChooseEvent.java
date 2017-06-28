11e6885643516ad2cf4eee54722eaf6eeae46660
amrithm98 <amrithchallenger@gmail.com>
2017-06-28 18:31:39 +0530

    Event chooser complete

--- app/src/main/java/amrith/com/volunteers/ChooseEvent.java
+++ app/src/main/java/amrith/com/volunteers/ChooseEvent.java
@@ -1,11 +1,20 @@
 package amrith.com.volunteers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
+import amrith.com.volunteers.Adapters.EventListAdapter;
+import amrith.com.volunteers.Adapters.FeedAdapter;
 import amrith.com.volunteers.Utils.ApiClient;
 import amrith.com.volunteers.Utils.Global;
 import amrith.com.volunteers.Utils.ProgressDialog;
@@ -16,11 +25,17 @@ import amrith.com.volunteers.models.Event;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
-public class ChooseEvent extends AppCompatActivity {
+public class ChooseEvent extends AppCompatActivity implements EventListAdapter.ItemClickListener{
 
     @BindView(R.id.rv_event)
     RecyclerView rvEvent;
+
+    public List<Event> eList;
+
+    public EventListAdapter eventListAdapter;
 
     ProgressDialog progressDialog;
     @Override
@@ -35,10 +50,36 @@ public class ChooseEvent extends AppCompatActivity {
             public void tokenObtained(String token) {
                 RestApiInterface service = ApiClient.getService();
                 Call<List<Event>> call=service.getEventList(token, Global.uid);
+                call.enqueue(new Callback<List<Event>>() {
+                    @Override
+                    public void onResponse(Call<List<Event>> call, Response<List<Event>> response) {
+                        progressDialog.disMissProgressDialog();
+                        eList= (ArrayList<Event>) response.body();
+                        eventListAdapter=new EventListAdapter(getApplicationContext(),eList);
+                        rvEvent.setAdapter(eventListAdapter);
+                        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getApplicationContext());
+                        rvEvent.setLayoutManager(linearLayoutManager);
+                        eventListAdapter.notifyDataSetChanged();
+                    }
 
+                    @Override
+                    public void onFailure(Call<List<Event>> call, Throwable t) {
+                        progressDialog.disMissProgressDialog();
+                        Log.d("FailItems",t.toString());
+                        Toast.makeText(getApplicationContext(),"Unable to Load",Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
 
 
+    }
+
+    @Override
+    public void onClick(View view, int position) {
+        Event event=eList.get(position);
+        int eventId=event.id;
+        Global.eventId=eventId;
+        startActivity(new Intent(ChooseEvent.this,MainActivity.class));
     }
 }
