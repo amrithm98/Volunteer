4ed9130d0651dffd6e2224654ab5366ac121d91d
amrithm98 <amrithchallenger@gmail.com>
2017-07-11 12:12:19 +0530

    Get volunteer list done

--- app/src/main/java/amrith/com/volunteers/Fragments/AddVolunteer.java
+++ app/src/main/java/amrith/com/volunteers/Fragments/AddVolunteer.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.PagerAdapter;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,12 +22,17 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+import amrith.com.volunteers.Adapters.FeedAdapter;
+import amrith.com.volunteers.Adapters.VoltListAdapter;
 import amrith.com.volunteers.R;
+import amrith.com.volunteers.RegisterActivity;
 import amrith.com.volunteers.Utils.ApiClient;
+import amrith.com.volunteers.Utils.ProgressDialog;
 import amrith.com.volunteers.Utils.RestApiInterface;
 import amrith.com.volunteers.Utils.TokenUtil;
 import amrith.com.volunteers.models.Admin;
 import amrith.com.volunteers.models.College;
+import amrith.com.volunteers.models.Feed;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
@@ -38,7 +45,17 @@ public class AddVolunteer extends Fragment{
     @BindView(R.id.clg_spinner)
     Spinner spinner;
 
+    @BindView(R.id.rv_volt)
+    RecyclerView rvVolt;
+
+    List<Admin> adminList;
+
+    ProgressDialog progressDialog;
+
     HashMap<String ,Integer> collegeList;
+
+    VoltListAdapter voltListAdapter;
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
@@ -76,6 +93,9 @@ public class AddVolunteer extends Fragment{
                     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                         String college=spinner.getItemAtPosition(position).toString();
                         getPeople(collegeList.get(college));
+                        adminList=new ArrayList<>();
+                        progressDialog=new ProgressDialog(getActivity());
+                        progressDialog.showProgressDialog();
                     }
 
                     @Override
@@ -102,14 +122,23 @@ public class AddVolunteer extends Fragment{
                 call.enqueue(new Callback<List<Admin>>() {
                     @Override
                     public void onResponse(Call<List<Admin>> call, Response<List<Admin>> response) {
+                        progressDialog.disMissProgressDialog();
                         if(response.code()==200)
                         {
                             Toast.makeText(getActivity(),"Got Volunteers in the college",Toast.LENGTH_SHORT).show();
+                            List<Admin> admins = (List<Admin>) response.body();
+                            adminList=admins;
+                            voltListAdapter=new VoltListAdapter(getActivity(),adminList);
+                            rvVolt.setAdapter(voltListAdapter);
+                            LinearLayoutManager linearLayoutManager=new LinearLayoutManager(getActivity());
+                            rvVolt.setLayoutManager(linearLayoutManager);
+                            voltListAdapter.notifyDataSetChanged();
                         }
                     }
 
                     @Override
                     public void onFailure(Call<List<Admin>> call, Throwable t) {
+                        progressDialog.disMissProgressDialog();
 
                     }
                 });
