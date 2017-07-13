aef26745099517ca5bea70d7f0ad66dc2555e8a7
amrithm98 <amrithchallenger@gmail.com>
2017-07-13 16:11:30 +0530

    Team Activity in the making

--- app/src/main/java/amrith/com/volunteers/TeamActivity.java
+++ app/src/main/java/amrith/com/volunteers/TeamActivity.java
@@ -1,13 +1,31 @@
 package amrith.com.volunteers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
+
+import amrith.com.volunteers.Utils.Global;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TeamActivity extends AppCompatActivity {
+
+    @BindView(R.id.team_name)
+    TextView teamName;
+
+    @BindView(R.id.rv_volunteers)
+    RecyclerView rvVolunteers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Intent intent=getIntent();
+        int teamId=intent.getIntExtra("team",0);
         setContentView(R.layout.activity_team);
+        ButterKnife.bind(this);
+        teamName.setText(Global.teamList.get(teamId));
+
     }
 }
